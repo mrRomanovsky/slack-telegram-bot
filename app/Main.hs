5205b81f17479fd4e92d5e5d0482f5db1cd0720a
mrRomanovsky <rmansorokin@gmail.com>
2018-07-17 15:05:16 +0300

    implemented reading config

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Config
 import Data.Aeson
+import qualified Data.ByteString.Lazy as B
 
 main :: IO ()
 main = do
   putStrLn "hello world"
+  strConf <- B.readFile "app/config/config.txt"
+  let conf = eitherDecode strConf :: Either String Config
+  either putStrLn print conf
