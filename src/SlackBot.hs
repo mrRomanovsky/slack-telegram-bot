ab19a5bfc344789e4752d71d89c5f8b319f6361b
Andrew Romanovsky <rmansorokin@gmail.com>
2018-07-21 20:41:33 +0300

    implemented basic echo function

--- src/SlackBot.hs
+++ src/SlackBot.hs
@@ -21,6 +21,22 @@ data SlackConfig = SlackConfig {token :: String,
 instance FromJSON SlackConfig
 instance ToJSON SlackConfig
 
+echoLastMessage :: SlackConfig -> IO ()
+echoLastMessage sc = do
+  lastMessages <- getMessages sc
+  let lastText = maybe Nothing  getLastMessageText lastMessages
+  maybe (return ()) (sendText sc) lastText
+
+getLastMessageText :: [SlackMessage] -> Maybe String
+getLastMessageText [] = Nothing
+getLastMessageText l = Just $ text $ last l
+
+sendText :: SlackConfig -> String -> IO ()
+sendText sc@SlackConfig{token = t, channel = c} txt =
+  send "https://slack.com/api/chat.postMessage" $ RequestBodyBS $ pack $
+   "{\"token\":\"" ++ t ++ "\",\"channel\":\"" ++ c
+   ++ "\",\"text\":\"" ++ txt ++ "\"}"
+
 getMessages :: SlackConfig -> IO (Maybe [SlackMessage])
 getMessages SlackConfig{token = t, channel = c} = do
   let getHistory = "https://slack.com/api/channels.history?token="
